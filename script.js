79c9d9bbc6b4abbc0bb022b3e6fabb110e754d17
Luz <luz.d.saucedo@kp.org>
2024-02-20 20:01:38 -0600

    Practica 4 Parte 1

--- script.js
+++ script.js
@@ -1,14 +1,37 @@
-var miVariable = 5; //numerico
+var articuloInput = document.getElementById('articuloTXT');
+var montoInput = document.getElementById('montoTXT');
 
-var campoTexto = document.getElementById("miTxt");
-var texto = ""; // campoTexto.value = "";
+var articulo = '';
+var monto = 0;
 
-function miFuncion() {
-    // alert(y);
-    texto = campoTexto.value;
-    alert(texto);
+
+var total = 0;
+
+
+const seccionTicket = document.getElementById('ticket');
+//Agregar
+function agregar() {
+    
+    articulo = articuloInput.value;
+//parseInt: Para convertir valores String en Numericos
+    monto = parseInt(montoInput.value); 
+
+    total = total + monto;
+
+    //Crear Elemento HTML
+
+    let elementoTicket = document.createElement('p');
+
+    elementoTicket.innerHTML = articulo + '......... $' + monto;
+
+    //inserta elemento HTML en la pagina
+    seccionTicket.appendChild(elementoTicket);
+
+    //limpia campos de texto
+    limpiar();
 }
 
-function limpiar(){
-    campoTexto.value = "";
+function limpiar() {
+    articuloInput.value = '';
+    montoInput.value = '';
 }
